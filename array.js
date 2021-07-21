a9582f3f7b32c077aab967798520282a9a78cbe2
JackJackHuo <86769801+JackJackHuo@users.noreply.github.com>
2021-07-22 02:18:56 +0800

    Add files via upload

---
+++ array.js
@@ -0,0 +1,64 @@
+// 物件取值
+
+// const post = {
+//     name : "jack",
+//     address : "123street",
+// }
+// const post1 = {
+//     name : "kevin",
+//     address : "123street"
+// }
+
+// console.log(post.name)//物件第一種取值方式
+// console.log(post1['address']); //物件第二種取值方式
+
+
+// let x = [post,post1]
+// console.log(x)
+// ============================================================
+//陣列資料修改
+
+// let card1 = {
+//     name:"Jack",
+//     gender:"male"
+// }
+// let card2 = {
+//     name:"Kevin",
+//     gender:"male"
+// }
+// let card3 = {
+//     name:"Jennifer",
+//     gender:"female"
+// }
+// let book = [card1,card2,card3]
+
+// console.log(book[0]);
+// console.log(book[1]);
+// console.log(book[2]);
+
+// let added = book.unshift("hello") //Array增加第一筆資料，其餘往後推
+// console.log(added);//回傳陣列長度
+
+// let poped = book.pop()//Array刪除最後一筆資料
+// console.log(poped); //回傳刪除的資料
+
+// let deleted = book.splice(2,1)
+// console.log(book);
+// console.log(deleted);
+
+// spliceAdded = book.splice(1 , 0 , 'Justin' , 'Kobe') //從index = 1 增加0後面的元素
+// console.log(book);
+// ============================================================
+//陣列forEach函式
+
+// const myArray = [100 , 200 , 300]
+// myArray.forEach(function (item , index , array) {
+//     console.log(`${index} = ${item + 10}`)
+//     array[index] = item * 10
+//     console.log(myArray);
+// })
+
+
+
+
+
---
+++ this.js
@@ -0,0 +1,87 @@
+//誰調用了包含「this」的function，誰就是this
+
+// function hello() {
+//     console.log("name",this.name);
+    
+// }
+// const b = {name:"jack"}
+// const c = {name:"kevin"}
+
+// b.hellooo = hello
+// c.hellooo = hello
+
+// b.hellooo()
+// c.hellooo()
+
+// ==============================================
+// 用箭頭函式綁定this
+
+// class Car{
+//     constructor(carName){
+//         this.name = carName
+//     }
+//     start = () => {
+//         console.log("啟動",this.name);
+//     }
+// }
+
+
+// const myCar = new Car("保時捷")
+// myCar.start()
+
+// const myBus = {name:"公車"}
+// myBus.initiate = myCar.start
+// myBus.initiate()
+
+// ===================================================
+//call
+
+// function hello(years, address) {
+//     console.log(this.name)
+//     console.log(years, "歲")
+//     console.log("住址：", address)
+//   }
+//   const a = { name: "bruce" }
+//   const b = { name: "mike" }
+//   // bruce
+//   // 100歲
+//   hello.call(a, 100, "高雄市ＸＸＸ")
+//   // mike
+//   // 100歲
+//   hello.call(b, 100, "台北市ＸＸＸ")
+// ===================================================
+//apply
+
+// function hello(years, address) {
+//     console.log(this.name)
+//     console.log(years, "歲")
+//     console.log("住址：", address)
+//   }
+//   const a = { name: "bruce" }
+//   const b = { name: "mike" }
+//   // bruce
+//   // 100歲
+//   // 只是把參數改成用array收集
+//   hello.apply(a, [100, "高雄市ＸＸＸ"])
+//   // mike
+//   // 100歲
+//   hello.apply(b, [100, "台北市ＸＸＸ"])
+// ===================================================
+//bind
+
+// function hello(years, address) {
+//     console.log(this.name)
+//     console.log(years, "歲")
+//     console.log("住址：", address)
+//   }
+//   const a = { name: "bruce" }
+//   const b = { name: "mike" }
+//   // 返回了新函數～～～
+//   const newHelloA = hello.bind(a, 100, "高雄市ＸＸＸ")
+//   const newHelloB = hello.bind(b, 100, "台北市ＸＸＸ")
+//   // bruce
+//   // 100歲
+//   newHelloA()
+//   // mike
+//   // 100歲
+//   newHelloB()
