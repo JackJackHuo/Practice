2f07f1309e92e744fea4e487155a604469f9af3e
JackJackHuo <86769801+JackJackHuo@users.noreply.github.com>
2021-08-23 19:31:33 +0800

    Add files via upload

--- JS_Practice/constructor_prototype.js
+++ JS_Practice/constructor_prototype.js
@@ -13,11 +13,11 @@
 //     age: 18,
 //     height: "6'6"
 // }
-// ==========constructor function========================================
+console.log('==========constructor function========================================')
 function Player(name , age , height){
     this.name = name,
     this.age = age,
-    this.height = height   
+    this.height = height    
 }
 // Player.prototype = {
 //     constructor: Player,
@@ -46,45 +46,55 @@ console.log(player2)
 console.log(player3) 
 //Player {name: "Lebron James", age: 18, height: "6'6"}
 
-player1.shoot() //Kobe Bryant 投籃
-player2.dunk() //Michael Jordan 灌籃
-
-
-console.log(Player.prototype)
-console.log(player1.__proto__)
-
-// ==========class========================================
-
-// class Player{
-//     constructor(name , age , height){
-//         this.name = name,
-//         this.age = age,
-//         this.height = height   
-//     }
-//     shoot(){
-//         console.log(`${this.name} 投籃`)
-//     }
-//     dunk(){
-//         console.log(`${this.name} 灌籃`)
-//     }
-// }
-
-// let player1 = new Player('Kobe Bryant' , 23 , "6'9")
-// let player2 = new Player('Michael Jordan' , 29 , "6'7")
-// let player3 = new Player('Lebron James' , 18 , "6'6")
-
-// console.log(player1) 
-// //Player {name: "Kobe Bryant", age: 23, height: "6'9"}
-// console.log(player2) 
-// //Player {name: "Michael Jordan", age: 29, height: "6'7"}
-// console.log(player3) 
-// //Player {name: "Lebron James", age: 18, height: "6'6"}
-
 // player1.shoot() //Kobe Bryant 投籃
 // player2.dunk() //Michael Jordan 灌籃
 
 
+console.log(Player.prototype)
+console.log(player1.__proto__)
+console.log(Player)
 
+console.log('==========class========================================')
+
+class Players{
+    constructor(name , age , height){
+        this.name = name,
+        this.age = age,
+        this.height = height   
+    }
+    shoot(){
+        console.log(`${this.name} 投籃`)
+    }
+    dunk(){
+        console.log(`${this.name} 灌籃`)
+    }
+}
+// Players.prototype.shoot = function () {
+//     console.log(`${this.name} 投籃`)
+// }
+// Players.prototype.dunk = function () {
+//     console.log(`${this.name} 灌籃`)
+// }
+
+let player4 = new Players('Kobe Bryant' , 23 , "6'9")
+let player5 = new Players('Michael Jordan' , 29 , "6'7")
+let player6 = new Players('Lebron James' , 18 , "6'6")
+
+console.log(player4) 
+//Player {name: "Kobe Bryant", age: 23, height: "6'9"}
+console.log(player5) 
+//Player {name: "Michael Jordan", age: 29, height: "6'7"}
+console.log(player6) 
+//Player {name: "Lebron James", age: 18, height: "6'6"}
+
+// player4.shoot() //Kobe Bryant 投籃
+// player5.dunk() //Michael Jordan 灌籃
+
+
+
+console.log(Players.prototype)
+console.log(player4.__proto__)
+console.log(Players)
 
 
 
